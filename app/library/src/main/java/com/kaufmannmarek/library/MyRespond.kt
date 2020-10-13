814ff93111512674543c87b93febc93a71f5f126
Airso <airsoftman4747@gmail.com>
2020-10-13 17:18:47 +0200

    Renamed methods to avoid name conflicts

--- app/library/src/main/java/com/kaufmannmarek/library/MyRespond.kt
+++ app/library/src/main/java/com/kaufmannmarek/library/MyRespond.kt
@@ -3,7 +3,7 @@
 package com.kaufmannmarek.library
 
 /**
- * Creates an interface, which serves in case, when you need to call origin activity method, when the tasks in different class are done. To make this working, you must implement this abstract class in origin activity with method onRespondReceived. Set up the request code as you need. It serves as filter at the end of the process. Then you must pass this implementation to method of the class, where the other tasks are performed. Once the tasks are done and you got it´s result, you must call setResult, setResultOK or setResultBad. Afterwards the method onRespondReceived is called. You can implement this method as you need
+ * Creates an interface, which serves in case, when you need to call origin activity method, when the tasks in different class are done. To make this working, you must implement this abstract class in origin activity with method onRespondReceived. Set up the request code as you need. It serves as filter at the end of the process. Then you must pass this implementation to method of the class, where the other tasks are performed. Once the tasks are done and you got it´s result, you must call setTaskResult, setTaskResultOK or setTaskResultBad. Afterwards the method onRespondReceived is called. You can implement this method as you need
  */
 @Suppress("MemberVisibilityCanBePrivate")
 abstract class MyRespond {
@@ -18,7 +18,7 @@ abstract class MyRespond {
      * @param resultCode represents the result of the task
      * @param data are data, that needs to be passed to origin activity
      */
-    fun setResult(resultCode: Int, data: Any?) {
+    fun setTaskResult(resultCode: Int, data: Any?) {
         this.resultCode = resultCode
         onRespondReceived(this.requestCode, resultCode, data)
     }
@@ -28,8 +28,8 @@ abstract class MyRespond {
      *
      * @param resultCode represents the result of the task
      */
-    fun setResult(resultCode: Int) {
-        setResult(resultCode, null)
+    fun setTaskResult(resultCode: Int) {
+        setTaskResult(resultCode, null)
     }
 
     /**
@@ -37,15 +37,15 @@ abstract class MyRespond {
      *
      * @param data are data, that needs to be passed to origin activity
      */
-    fun setResultOK(data: Any?) {
-        setResult(RESULT_OK, data)
+    fun setTaskResultOK(data: Any?) {
+        setTaskResult(RESULT_OK, data)
     }
 
     /**
      * Sets OK result of the task
      */
-    fun setResultOK() {
-        setResultOK(null)
+    fun setTaskResultOK() {
+        setTaskResultOK(null)
     }
 
 
@@ -54,15 +54,15 @@ abstract class MyRespond {
      *
      * @param data are data, that needs to be passed to origin activity
      */
-    fun setResultBad(data: Any?) {
-        setResult(RESULT_BAD, data)
+    fun setTaskResultBad(data: Any?) {
+        setTaskResult(RESULT_BAD, data)
     }
 
     /**
      * Sets BAD result of the task
      */
-    fun setResultBad() {
-        setResultBad(null)
+    fun setTaskResultBad() {
+        setTaskResultBad(null)
     }
 
     /**
@@ -83,7 +83,7 @@ abstract class MyRespond {
      * This method is called in distinct class where the task is performed. This method must be implemented in origin class. You may implement it as you need.
      *
      * @param requestCode is code of performed operation. This is the argument, you have set before the passing the class instance to different class
-     * @param resultCode is result of operation. It is set by setResult, setResultOK (-1) or setResultBad (1) method
+     * @param resultCode is result of operation. It is set by setTaskResult, setTaskResultOK (-1) or setTaskResultBad (1) method
      * @param data is optional and it may contain any values or object. Can be null
      */
     abstract fun onRespondReceived(requestCode: Int, resultCode: Int, data: Any?)
